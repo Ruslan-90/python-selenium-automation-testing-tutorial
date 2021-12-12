2623608695c0d1ba75a266dedd964ddfb10a5318
Ruslan S <mr.ruslan.sultanov@gmail.com>
2021-12-12 22:51:41 +0300

    ref tests and login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,6 @@ from .locators import MainPageLocators
 
 
 class LoginPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
